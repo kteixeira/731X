e1c86aa3ceb6f313f829f3d7f24c76f4493f5d37
Kaio Leal <kaio.teco@gmail.com>
2015-06-27 18:57:45 -0300

    alteração sobre

--- sobre.php
+++ sobre.php
@@ -8,13 +8,13 @@
 		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
 	</head>
 	<body>
-		<?php include '/header-black.php'; ?>
+		<?php include 'header-black.php'; ?>
 			<div class = "container-part1">
 				<div id = "info">
 					<h1>S O B R E</h1><br/>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
-					aliqua.<br/><br/> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/><br/> 
-					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/><br/> 
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua.<br/><br/> Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.<br/><br/>
+					Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.<br/><br/>
 					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
 				</div>
 			</div>
