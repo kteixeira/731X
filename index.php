a78c2901ad1620307da569e3a1ec65fee23a199d
Gustavo Modena <gustavo.o.modena@gmail.com>
2015-06-28 04:12:07 -0300

    Slider added in Home

--- index.php
+++ index.php
@@ -3,57 +3,36 @@
 	<head>
 		<title>731X - Respeito e União</title>
 		<meta charset="UTF-8" />
-		<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-		<script type="text/javascript" src="js/jquery.cycle.all.js"></script>
 		<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script type="text/javascript" src="js/script.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"><!-- Link Swiper's CSS -->
+    	<link rel="stylesheet" href="bower_components/swiper/dist/css/swiper.min.css">
 		<link rel="stylesheet" type="text/css" href="css/style.css" />
 		<link rel="stylesheet" type="text/css" href="css/grid-min.css" />
-		<script type="text/javascript">
-			
-		</script>
 	</head>
-	<body>
-		
-		<div class="box">
-			<?php include 'header-white.php'; ?>
-			<span class="pager"></span>	
+<body>
 
-			<div class="slider">
-				<div class="slider-item">
-					<img src="img/lookbook/001.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/002.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/003.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/004.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/005.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/006.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/007.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-				<div class="slider-item">
-					<img src="img/lookbook/008.jpg" width="1424px"/>
-					<span class="pager"></span>
-				</div>
-			</div>
-		</div>
-	</body>
+	<?php include 'header-white.php'; ?>
+	<span class="pager"></span>	
+
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><div class="slides item-1"></div></div>
+            <div class="swiper-slide"><div class="slides item-2"></div></div>
+            <div class="swiper-slide"><div class="slides item-3"></div></div>
+            <div class="swiper-slide"><div class="slides item-4"></div></div>
+        </div>
+        <div class="swiper-pagination"></div>
+    </div>
+
+    <script src="bower_components/swiper/dist/js/swiper.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+	<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery.cycle.all.js"></script>
+    <script>
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+    </script>
+</body>
 </html>
