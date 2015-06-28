5d81eacf392b37d88047d2396bfc543d5cb0f967
Gustavo Modena <gustavo.o.modena@gmail.com>
2015-06-28 04:19:53 -0300

    Header black fixed

--- header-black.php
+++ header-black.php
@@ -1,25 +1,27 @@
-<div class="grid-container">
-	<div class="grid-100">
-		<nav id = "menu">
-			<img src="img/LOGO_HORIZONTAL_PRETO.png" width="150px"/>
-			
-			<ul>
-				<li>
-					<a href = "index.php">HOME</a>
-				</li>
-				<li>
-					<a href = "lookbook.php">LOOKBOOK</a>
-				</li>
-				<li>
-					<a href = "sobre.php">SOBRE</a>
-				</li>
-				<li>
-					<a href = "contato.php">CONTATO</a>
-				</li>
-				<li>
-					<a href="#" class = "but">LOJA VIRTUAL</a>
-				</li>
-			</ul>
-		</nav>
+<header class="header">
+	<div class="grid-container">
+		<div class="grid-100">
+			<nav id="menu-white" class="header__menu">
+				<img class="header__logo" src="img/LOGO_HORIZONTAL_BRANCO.png"/>
+				
+				<ul>
+					<li>
+						<a href="index.php">HOME</a>
+					</li>
+					<li>
+						<a href="lookbook.php">LOOKBOOK</a>
+					</li>
+					<li>
+						<a href="sobre.php">SOBRE</a>
+					</li>
+					<li>
+						<a href="contato.php">CONTATO</a>
+					</li>
+					<li>
+						<a href="#" class="header__loja-virtual">LOJA VIRTUAL</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	</div>
-</div>
+</header>
